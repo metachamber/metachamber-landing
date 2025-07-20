8ccd32be7e707aa8e6a08401e17ca2268c7692c4
Iman Gholami <imangholami77@gmail.com>
2025-07-20 15:20:45 -0700

    Deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(fileURLToPath(new URL('.', import.meta.url)), './src'),
